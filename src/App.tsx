a9109f873824cf2a7462917e6d9a134d239fea8f
20200471 <20200471@kuh.ac.kr>
2021-11-03 11:20:48 +0900

    mui 퍼블리싱 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,15 @@
 import React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Grid from '@mui/material/Grid';
-import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
-
-const Search = styled('div')(({ theme }) => ({
-	position: 'relative',
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	'&:hover': {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginLeft: 0,
-	width: '100%',
-	[theme.breakpoints.up('sm')]: {
-		marginLeft: theme.spacing(1),
-		width: 'auto',
-	},
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: '100%',
-	position: 'absolute',
-	pointerEvents: 'none',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: 'inherit',
-	'& .MuiInputBase-input': {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create('width'),
-		width: '100%',
-		[theme.breakpoints.up('sm')]: {
-			width: '20ch',
-			'&:focus': {
-				width: '28ch',
-			},
-		},
-	},
-}));
+import { InputAdornment } from '@mui/material';
 
 function App() {
 	return (
@@ -62,15 +17,6 @@ function App() {
 			<Box sx={{ flexGrow: 1, '& button': { m: 1 } }}>
 				<AppBar position="static" color="inherit">
 					<Toolbar>
-						{/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
 						<Typography
 							variant="h4"
 							component="div"
@@ -78,15 +24,21 @@ function App() {
 						>
 							blind
 						</Typography>
-						<Search>
-							<SearchIconWrapper>
-								<SearchIcon />
-							</SearchIconWrapper>
-							<StyledInputBase
-								placeholder="Search…"
-								inputProps={{ 'aria-label': 'search' }}
-							/>
-						</Search>
+
+						<TextField
+							id="outlined-basic"
+							label="관심있는 내용을 검색해보세요!"
+							variant="outlined"
+							size="small"
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								)
+							}}
+						/>
+
 						<Button color="error" size="large" variant="contained">
 							글쓰기
 						</Button>
