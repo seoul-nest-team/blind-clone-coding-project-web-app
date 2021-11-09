4204ac762983076c2b88310dd949738c066e1c45
20200471 <20200471@kuh.ac.kr>
2021-11-09 13:59:25 +0900

    redux 세팅

---
+++ src/common/store.ts
@@ -0,0 +1,7 @@
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({});
+const store = createStore(reducer, composeWithDevTools());
+
+export default store;
